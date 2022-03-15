8fb4c6cc53e4b5f157a9855df2fba3181f548539
Nicholas Reich <nick@umass.edu>
2022-03-15 14:00:40 -0400

    updates to validation phase analysis code.

--- code/validation-phase-analysis.R
+++ code/validation-phase-analysis.R
@@ -9,48 +9,65 @@ library(covidHubUtils)
 
 # TODO: 
 # - update data_issue_date after downloading more recent data, thru Omicron wave
-# - download Monday versions of data from Oct 2020 - Dec 2020 and add to main test data csv file
+# - download Monday versions of data from Oct 2020 - Dec 2020 and add to main test data csv file, change first_forecast_date
 
 ## define global parameters
 hosp_data_start_date <- ymd("2020-08-01")
 case_data_start_date <- ymd("2020-03-01")
-first_forecast_date <- ymd("2020-10-05") ## roughly start of winter wave in MA
+first_forecast_date <- ymd("2021-01-04") #ymd("2020-10-05") ## roughly start of winter wave in MA
 last_forecast_date <- ymd("2021-06-07") ## roughly start of summer trough in MA
 data_issue_date <- as.Date("2022-02-02") ## could update with newer data
 
-## load hosp truth data, both sets of case data
-hosp_truth <- load_truth(
+validation_forecast_dates <- seq.Date(first_forecast_date, last_forecast_date, by = "1 week")
+
+## load hosp truth data
+hosp_final <- load_truth(
   truth_source = "HealthData",
   target_variable = "inc hosp",
-  locations = "25") %>% 
-  filter(target_end_date <= forecast_date, target_end_date >= hosp_data_start_date)
+  locations = "25",
+  as_of = data_issue_date) %>% 
+  filter(
+    ## only need data until last forecast date
+    target_end_date <= last_forecast_date, 
+    ## data should start ~1wk prior to the first date, to allow for weekly differencing
+    target_end_date >= hosp_data_start_date-8) %>%
+  as_tsibble() %>% 
+  select(target_end_date, hosps = value) %>% 
+  tsibble::fill_gaps()
 
-report_date_case_truth <- load_truth(
+## load ReportCaseFinal data
+report_case_final <- load_truth(
   truth_source = "JHU",
   target_variable = "inc case",
   temporal_resolution = "daily",
-  locations = "25") %>% 
-  filter(target_end_date <= forecast_date, target_end_date >= case_data_start_date, value >= 0)
+  locations = "25",
+  as_of = data_issue_date) %>% 
+  filter(
+    target_end_date <= forecast_date, 
+    target_end_date >= case_data_start_date, 
+    ## note: filtering out values with <0 reported case values
+    value >= 0) %>% 
+  select(target_end_date, report_cases = value) %>%  #change variable names
+  as_tsibble() %>% 
+  tsibble::fill_gaps()
 
-test_date_case_truth <- read.csv("../csv-data/MA-DPH-csvdata-covid-2022-02-02.csv") %>%  #chose latest issue date from csv files
+
+## load TestCaseFinal data
+test_case_final <- read.csv("../csv-data/MA-DPH-csvdata-covid-2022-02-02.csv") %>%  #chose latest issue date from csv files
   mutate(issue_date = data_issue_date) %>% 
-  mutate(Date = substr(Date, 1, 10)) %>% 
-  mutate(Date = as.Date(Date)) %>% 
-  filter(Date <= forecast_date, Date >= case_data_start_date)
-
-ts_hosp <- hosp_truth %>%
-  as_tsibble() %>% 
-  select(target_end_date, hosp_val = value) %>% 
-  tsibble::fill_gaps()
-
-ts_report_date_case <- report_date_case_truth %>% 
-  select(target_end_date, report_case_val = value) %>%  #change variable names
-  as_tsibble() %>% 
-  tsibble::fill_gaps()
-
-ts_test_date_case <- test_date_case_truth %>% 
-  select(target_end_date = Date, test_case_val = Positive.New) %>% 
+  mutate(target_end_date = ymd(substr(Date, 1, 10))) %>% 
+  filter(
+    target_end_date <= forecast_date, 
+    target_end_date >= case_data_start_date) %>% 
+  select(target_end_date, test_case = Positive.New) %>% 
   as_tsibble() %>% 
   tsibble::fill_gaps() 
 
+
 ## loop through forecast dates
+
+## for a forecastMonday
+##    - extract as-of data for TestCases 
+##    - fit all models
+
+
