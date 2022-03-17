9d21361813d976cbcdf5432a0eac1ad8e898ee53
eycramer <ecramer@umass.edu>
2022-03-17 11:01:06 -0400

    include hospitalization lags

--- hosp_arima_forecast_function.R
+++ hosp_arima_forecast_function.R
@@ -7,7 +7,16 @@ library(covidHubUtils)
 library(fable)
 
 
-hosp_arima_forecast_function <- function(forecast_date, hosp_data_start_date, case_data_source, case_data_start_date, data_issue_date) {
+
+# define transformation and inverse of transformation
+fourth_rt<- function(x) {x^(1/4)}
+truncated_inv_fourth_rt <- function(x){
+  pmax(0,x)^4
+}
+fourth_rt_transformation <- new_transformation(fourth_rt, truncated_inv_fourth_rt)
+
+
+hosp_arima_forecast_function <- function(P, p, forecast_date, hosp_data_start_date, case_data_source, case_data_start_date, data_issue_date) {
   
   hosp_truth <- load_truth(
     truth_source = "HealthData",
@@ -66,7 +75,7 @@ hosp_arima_forecast_function <- function(forecast_date, hosp_data_start_date, ca
   
   
   model_cases_by_source <- merge_all_ts[c("target_end_date", "temp_col")] %>% 
-    model(ARIMA(log(temp_col+1)))
+    model(ARIMA(fourth_rt_transformation(temp_col)))
   
   report(model_cases_by_source)
   
@@ -80,37 +89,82 @@ hosp_arima_forecast_function <- function(forecast_date, hosp_data_start_date, ca
   all_cases_by_date <-  merge_all_ts[c("target_end_date", "temp_col")] %>%
     bind_rows(select(as_tsibble(new_case_by_source_data), target_end_date, temp_col = `.mean`))
   
+  
   hosps_and_case_by_date <- ts_hosp %>%
     full_join(all_cases_by_date) %>%
-    mutate(case_lag5 = lag(temp_col, 5),
-           case_lag10 = lag(temp_col, 10),
+    mutate(case_lag1 = lag(temp_col, 1),
+           case_lag2 = lag(temp_col, 2),
+           case_lag7 = lag(temp_col, 7),
+           case_lag8 = lag(temp_col, 8),
+           case_lag9 = lag(temp_col, 9),
+           case_lag14 = lag(temp_col, 14),
            case_lag15 = lag(temp_col, 15),
-           case_lag20 = lag(temp_col, 20)) %>%
+           case_lag21 = lag(temp_col, 21),
+           hosp_lag1 = lag(hosp_val, 1),
+           hosp_lag2 = lag(hosp_val, 2),
+           hosp_lag3 = lag(hosp_val, 3),
+           hosp_lag7 = lag(hosp_val, 7),
+           hosp_lag8 = lag(hosp_val, 8),
+           hosp_lag9 = lag(hosp_val, 9),
+           hosp_lag14 = lag(hosp_val, 14),
+           hosp_lag15 = lag(hosp_val, 15),
+           hosp_lag16 = lag(hosp_val, 16),
+           hosp_lag17 = lag(hosp_val, 17),
+           hosp_lag21 = lag(hosp_val, 21),
+           hosp_lag22 = lag(hosp_val, 22),
+           hosp_lag23 = lag(hosp_val, 23),
+           hosp_lag24 = lag(hosp_val, 24)) %>%
     ## filter comes last to make sure we have lags early in the filtered dataset
     filter(target_end_date >= hosp_data_start_date)
+  
+
+  response_var <- "fourth_rt_transformation(hosp_val)"
+  
+  formula <- paste0(response_var, " ~ ",
+                    paste0("hosp_lag", seq_len(p), collapse = " + "), " + ", paste0("case_lag", seq_len(p), collapse = " + "))
+  seasonal_terms <- ""
+  for (seasonal_lag in seq_len(P)) {
+    seasonal_terms <- paste0(
+      seasonal_terms, " + ",
+      paste0("hosp_lag", seq(from = 0, length = p + 1) + seasonal_lag * 7, collapse = " + "), " + ",
+      paste0("case_lag", seq(from = 0, length = p + 1) + seasonal_lag * 7, collapse = " + "))
+  }
+  formula <- paste0(formula, seasonal_terms)
+
+  print(formula)
   
   #need to figure out why this is LM with arima errors.  
   hosps_and_case_model <- hosps_and_case_by_date %>%
     filter(target_end_date <= forecast_date) %>%
-    model(ARIMA(log(hosp_val+1) ~ case_lag5 + case_lag10 + case_lag15 + case_lag20))
+    model(ARIMA(as.formula(formula)))
   
   report(hosps_and_case_model)
   
-  hosps_and_case_model %>% 
-    forecast(new_data = filter(hosps_and_case_by_date, target_end_date > forecast_date))  %>%
+hosp_forecast_df <- hosps_and_case_model %>% 
+    forecast(new_data = filter(hosps_and_case_by_date, target_end_date > forecast_date), point_forecast = list(.mean = mean, .median = median)) 
+  
+hosp_forecast_df %>%
     autoplot(hosps_and_case_by_date) +
     labs(y = "Hospitalizations", title = paste("ARIMA Forecast of hospitalizations using cases by", case_data_source, "date"))
+
+return(hosp_forecast_df)
 }
 
+forecast(h=28, point_forecast = list(.mean = mean, .median = median))
 
-hosp_arima_forecast_function(forecast_date <- as.Date("2021-06-01"),
+
+hosp_arima_forecast_function(P = 2,
+                             p = 1, 
+                             forecast_date <- as.Date("2021-06-01"),
                              hosp_data_start_date <- as.Date("2020-08-01"),
                              case_data_source = "Report",
                              case_data_start_date <- as.Date("2020-03-01"),
                              data_issue_date = as.Date("2022-02-02"))
 
 
-hosp_arima_forecast_function(forecast_date <- as.Date("2021-06-01"),
+hosp_arima_forecast_function(P = 2,
+                             p = 1, 
+                             forecast_date <- as.Date("2021-06-01"),
                              hosp_data_start_date <- as.Date("2020-08-01"),
                              case_data_source = "Test",
                              case_data_start_date <- as.Date("2020-03-01"),
