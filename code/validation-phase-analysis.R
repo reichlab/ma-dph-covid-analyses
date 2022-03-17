0794f31608f8312dd6095d085d91f54f10645725
Nicholas Reich <nick@umass.edu>
2022-03-17 15:50:06 -0400

    updating validation phase analysis script based on comments from call today.

--- code/validation-phase-analysis.R
+++ code/validation-phase-analysis.R
@@ -11,11 +11,16 @@ library(covidData)
 # TODO: 
 # - download Monday versions of data from Oct 2020 - Dec 2020 and add to main test data csv file, change first_forecast_date
 # - fill in missing datapoints?
+# - add back in d=0:1, D=0:1
 
-## define global parameters
-hosp_data_start_date <- ymd("2020-08-01")
-case_data_start_date <- ymd("2020-03-01")
-first_forecast_date <- ymd("2021-01-04") #ymd("2020-10-05") ## roughly start of winter wave in MA
+### define global parameters
+
+## choosing this so that with a 1 week differencing the modeling might start around Aug 1
+## also, there were odd data anomalies in hospitalizations on July 21/22
+data_start_date <- ymd("2020-07-24")
+
+## roughly start of winter wave in MA, also give us a few weeks of data to fit the model on early
+first_forecast_date <- ymd("2021-01-04") #ymd("2020-10-05") 
 last_forecast_date <- ymd("2021-06-07") ## roughly start of summer trough in MA
 data_analysis_date <- as.Date("2022-03-14") ## could update with newer data
 
@@ -26,12 +31,13 @@ hosp_final <- load_truth(
   truth_source = "HealthData",
   target_variable = "inc hosp",
   locations = "25",
+  data_location = "covidData",
   as_of = data_analysis_date) %>% 
   filter(
     ## only need data until last forecast date
-    target_end_date <= last_forecast_date, 
+    target_end_date <= last_forecast_date,
     ## data should start ~1wk prior to the first date, to allow for weekly differencing
-    target_end_date >= hosp_data_start_date-8) %>%
+    target_end_date >= data_start_date) %>%
   select(target_end_date, hosps = value) %>% 
   as_tsibble(index = target_end_date) %>% 
   tsibble::fill_gaps()
@@ -42,10 +48,11 @@ report_case_final <- load_truth(
   target_variable = "inc case",
   temporal_resolution = "daily",
   locations = "25",
+  data_location = "covidData",
   as_of = data_analysis_date) %>% 
   filter(
     target_end_date <= last_forecast_date, 
-    target_end_date >= case_data_start_date, 
+    target_end_date >= data_start_date, 
     ## note: filtering out values with <0 reported case values
     value >= 0) %>% 
   select(target_end_date, report_cases = value) %>%  #change variable names
@@ -59,28 +66,34 @@ test_case_final <- read_csv("csv-data/MA-DPH-covid-alldata.csv") %>%
   mutate(target_end_date = test_date) %>% 
   filter(
     target_end_date <= last_forecast_date, 
-    target_end_date >= case_data_start_date) %>% 
+    target_end_date >= data_start_date) %>% 
   select(target_end_date, test_case_final = new_positive, issue_date) %>% 
   as_tsibble(index = target_end_date) %>% 
   tsibble::fill_gaps() 
 
 
 # set up variations on sarima specifications to consider in validation phase
-sarima_variations <- expand.grid(p=0:3, d=0:1, P=0:3, D=0:1)
+sarima_variations <- expand.grid(p=0:3, d=0, P=0:3, D=0) # eventually will use d=0:1, D=0:1
 ## loop through forecast dates
 for(forecast_date in validation_forecast_dates){
   
   ##  extract as-of data for TestCases 
   test_case_realtime <- read_csv("csv-data/MA-DPH-covid-alldata.csv") %>%  #chose latest issue date from csv files
-    filter(issue_date == forecast_date) %>% 
-    mutate(target_end_date = ymd(substr(Date, 1, 10))) %>% 
-    filter(
-      target_end_date <= forecast_date, 
-      target_end_date >= case_data_start_date) %>% 
-    select(target_end_date, test_case_realtime = Positive.New) %>% 
+    rename(target_end_date = test_date) %>%
+    filter(issue_date == forecast_date,
+           target_end_date <= forecast_date, 
+           target_end_date >= data_start_date) %>% 
+    select(target_end_date, test_case_realtime = new_positive) %>% 
     as_tsibble(index = target_end_date) %>% 
     tsibble::fill_gaps() 
-  
+
+  all_data <- hosp_final %>%
+    left_join(report_case_final) %>%
+    left_join(test_case_final) %>%
+    left_join(test_case_realtime) %>%
+    filter(target_end_date < forecast_date) %>%
+    select(-issue_date)
+    
   for (i in seq_len(nrow(sarima_variations))) {
     p <- sarima_variations$p[i]
     d <- sarima_variations$d[i]
@@ -88,16 +101,16 @@ for(forecast_date in validation_forecast_dates){
     D <- sarima_variations$D[i]
     
     ## fit all models
-    model1_solo <- arima_hosp_forecasts(hosp_data, p=p, d=d, P=P, D=D)
+    model1_solo <- arima_hosp_forecasts(all_data, p=p, d=d, P=P, D=D)
     # save model1_solo results
     
-    model2_reportcasefinal <- arima_hosp_forecasts(hosp_data, report_case_final, p=p, d=d, P=P, D=D)
+    model2_reportcasefinal <- arima_hosp_forecasts(all_data, case_col="report_case_final", p=p, d=d, P=P, D=D)
     # save model2_reportcasefinal results
     
-    model3a_testcasefinal <- arima_hosp_forecasts(hosp_data, test_case_final, p=p, d=d, P=P, D=D)
+    model3a_testcasefinal <- arima_hosp_forecasts(all_data, case_col="test_case_final", p=p, d=d, P=P, D=D)
     # save model3a_testcasefinal results
     
-    model3b_testcaserealtime <- arima_hosp_forecasts(hosp_data, test_case_realtime, p=p, d=d, P=P, D=D)
+    model3b_testcaserealtime <- arima_hosp_forecasts(all_data, case_col="test_case_realtime", p=p, d=d, P=P, D=D)
     # save model3b_testcaserealtime results
     
   }
