fe0b3e6b0c355d725b985fc605d4ba42ebd01d4c
Nicholas Reich <nick@umass.edu>
2022-04-22 11:53:27 -0400

    adding CA data and code.

---
+++ code/download-data-ca.R
@@ -0,0 +1,24 @@
+## code for downloading CA data
+## GitHub directory with lots of files: https://github.com/cagov/covid-static-data/tree/main/data/dashboard/confirmed-cases
+## GitHub JSON file for all of CA: https://github.com/cagov/covid-static-data/blob/main/data/dashboard/confirmed-cases/california.json
+
+## 2022-04-22: ONLY DOWNLOADING FINAL DATA, NOT WORRYING ABOUT VERIONS
+
+library(jsonlite)
+library(tidyverse)
+
+ca_dat <- read_json("https://github.com/cagov/covid-static-data/raw/main/data/dashboard/confirmed-cases/california.json",
+                 simplifyVector = TRUE)
+
+ca_test_date <- ca_dat$data$time_series$CONFIRMED_CASES_EPISODE_DATE$VALUES %>%
+  rename(test_date = DATE,
+         new_positive = VALUE) %>%
+  mutate(issue_date = ca_dat$meta$PUBLISHED_DATE)
+
+ca_report_date <- ca_dat$data$time_series$CONFIRMED_CASES_REPORTED_DATE$VALUES %>%
+  rename(report_date = DATE,
+         new_positive = VALUE) %>%
+  mutate(issue_date = ca_dat$meta$PUBLISHED_DATE)
+
+write_csv(ca_test_date, file = paste0("csv-data/CA-DPH-testdate-covid-", ca_dat$meta$PUBLISHED_DATE,".csv"))
+write_csv(ca_report_date, file = paste0("csv-data/CA-DPH-reportdate-covid-", ca_dat$meta$PUBLISHED_DATE,".csv"))
