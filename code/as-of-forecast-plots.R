3ff5e2505725f94c88f512e921d863d05f5210c9
Nicholas Reich <nick@umass.edu>
2022-06-10 12:31:20 -0400

    commit code for all plots.

--- code/as-of-forecast-plots.R
+++ code/as-of-forecast-plots.R
@@ -224,3 +224,14 @@ pdf(file = "plots/ca-202107.pdf", height=15, width=8)
 cowplot::plot_grid(ca1, ca2, ca3, nrow=3)
 cowplot::plot_grid(ma1, ma2, ma3, nrow=3)
 dev.off()
+
+
+forecast_dates <- as.character(seq.Date(as.Date("2020-12-07"), as.Date("2022-04-25"), by="7 days")) # c("2021-07-12", "2021-07-19", "2021-07-26")
+states <- c("ca", "ma")
+pdf(file = "plots/all-dates.pdf", height=5, width=8)
+for (state in states){
+  for(forecast_date in forecast_dates){
+    print(plot_case_hosp_forecasts(forecast_date = forecast_date, state=state))
+  }
+}
+dev.off()
